2071bc148718c0595c7708507c3d8ca705d0527b
Anderson <andersonmtv07@gmail.com>
2023-03-27 02:47:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+
+    {
+        int numero = 0;
+
+        int quantidadePares = 0;
+        int quantidadeImpares = 0;
+        int quantidadePositivos = 0;
+        int quantidadeNegativos = 0;
+
+        for (int i = 0; i < 5; ++i)
+        {
+            numero = int.Parse(ReadLine());
+            if (numero % 2 == 0) { ++quantidadePares; }
+            else { ++quantidadeImpares; }
+            if (numero > 0) { ++quantidadePositivos; }
+            else if (numero < 0) { ++quantidadeNegativos; }
+        }
+
+        WriteLine($"{quantidadePares} par(es)");
+
+        WriteLine($"{quantidadeImpares} impar(es)");
+
+        WriteLine($"{quantidadePositivos} positivo(s)");
+
+        WriteLine($"{quantidadeNegativos} negativo(s)");
+
+    }
+}
